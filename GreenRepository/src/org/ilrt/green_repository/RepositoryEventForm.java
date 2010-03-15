44a3c29c4bb57edd1909e459ec0af06e8cb5938a
phil.cross@bristol.ac.uk <phil.cross@bristol.ac.uk@482708ba-fdbe-11dd-9fdb-3debd5008f28>
2010-03-15 12:03:16 +0000

    Work in progress - committed for backup

---
+++ GreenRepository/src/org/ilrt/green_repository/RepositoryEventForm.java
@@ -0,0 +1,166 @@
+/**
+ * Copyright (c) 2010, University of Bristol
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1) Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2) Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3) Neither the names of the University of Bristol and the
+ *    University of Manchester nor the names of their
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package org.ilrt.green_repository;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.Date;
+
+/**
+ *
+ * @author Phil Cross (phil.cross@bristol.ac.uk)
+ */
+public class RepositoryEventForm {
+
+    private String eventId;
+    private String title;
+    private String startDate;
+    private String endDate;
+    private String description;
+    private String location;
+    private String eventUrl;
+    private String addButton;
+    private String updateButton;
+    private String cancelButton;
+
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    // Will only exist for existing events
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    // Return startDate as Date obj
+    public Date getStartDateObj() {
+        Date startDateObj = null;
+        if (startDate != null) {
+            try {
+                startDateObj = dateFormat.parse(startDate);
+            } catch(ParseException pe) {
+                startDateObj = null;
+            }
+        }
+        return startDateObj;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    // Return endDate as Date obj
+    public Date getEndDateObj() {
+        Date endDateObj = null;
+        if (endDate != null) {
+            try {
+                endDateObj = dateFormat.parse(endDate);
+            } catch(ParseException pe) {
+                endDateObj = null;
+            }
+        }
+        return endDateObj;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getEventUrl() {
+        return eventUrl;
+    }
+
+    public void setEventUrl(String eventUrl) {
+        this.eventUrl = eventUrl;
+    }
+
+    public String getAddButton() {
+        return addButton;
+    }
+
+    public void setAddButton(String addButton) {
+        this.addButton = addButton;
+    }
+
+    public String getUpdateButton() {
+        return updateButton;
+    }
+
+    public void setUpdateButton(String updateButton) {
+        this.updateButton = updateButton;
+    }
+
+    public String getCancelButton() {
+        return cancelButton;
+    }
+
+    public void setCancelButton(String cancelButton) {
+        this.cancelButton = cancelButton;
+    }
+}
---
+++ GreenRepository/src/org/ilrt/green_repository/domain/RepositoryEvent.java
@@ -0,0 +1,170 @@
+/**
+ * Copyright (c) 2010, University of Bristol
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1) Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2) Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3) Neither the names of the University of Bristol and the
+ *    University of Manchester nor the names of their
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+package org.ilrt.green_repository.domain;
+
+import org.ilrt.green_repository.RepositoryEventForm;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+
+/**
+ *
+ * @author Phil Cross (phil.cross@bristol.ac.uk)
+ */
+@Entity
+@Table(name = "REPOSITORY_EVENT")
+public class RepositoryEvent implements Serializable {
+
+    public RepositoryEvent() {
+    }
+
+    public RepositoryEvent(RepositoryEventForm form) {
+        this.eventId = form.getEventId();
+        this.title = form.getTitle();
+        this.startDate = form.getStartDate();
+        this.endDate = form.getEndDate();
+        this.description = form.getDescription();
+        this.location = form.getLocation();
+        this.eventUrl = form.getEventUrl();
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getEventUrl() {
+        return eventUrl;
+    }
+
+    public void setEventUrl(String eventUrl) {
+        this.eventUrl = eventUrl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RepositoryEvent)) return false;
+
+        RepositoryEvent event = (RepositoryEvent) o;
+        return eventId.equals(event.getEventId());
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        result = eventId.hashCode();
+        result = 31 * result + title.hashCode();
+        return result;
+    }
+
+    @Id
+    @Column(name = "EVENTID")
+    private String eventId;
+
+    @Column(name = "TITLE", nullable = false)
+    private String title;
+
+    @Column(name = "STARTDATE", nullable = true)
+    private String startDate;
+
+    @Column(name = "ENDDATE", nullable = true)
+    private String endDate;
+
+    @Column(name = "DESCRIPTION", nullable = true)
+    private String description;
+
+    @Column(name = "LOCATION", nullable = true)
+    private String location;
+
+    @Column(name = "EVENTURL", nullable = true)
+    private String eventUrl;
+
+
+}
