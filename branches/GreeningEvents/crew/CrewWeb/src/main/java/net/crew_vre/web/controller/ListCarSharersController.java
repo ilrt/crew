ba70ee1488785e057af9d1ef6e94ff146fe4ca2f
phil.cross@bristol.ac.uk <phil.cross@bristol.ac.uk@482708ba-fdbe-11dd-9fdb-3debd5008f28>
2010-07-25 19:12:08 +0000

    Working on car sharer code

---
+++ branches/GreeningEvents/crew/CrewWeb/src/main/java/net/crew_vre/web/controller/ListCarSharersController.java
@@ -0,0 +1,301 @@
+/**
+ * Copyright (c) 2008-2009, University of Bristol
+ * Copyright (c) 2008-2009, University of Manchester
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1) Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2) Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3) Neither the names of the University of Bristol and the
+ *    University of Manchester nor the names of their
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+package net.crew_vre.web.controller;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.ilrt.dibden.facade.UserManagementFacade;
+import org.ilrt.dibden.domain.User;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author Phil Cross (phil.cross@bristol.ac.uk)
+ */
+public class ListCarSharersController implements Controller {
+
+
+
+    private Logger logger = Logger.getLogger("net.crew_vre.web.controller.ListCarSharersController");
+
+    private UserManagementFacade userManagementFacade;
+    private String postcodeMapDirName;
+    String postcodesPath;
+
+    private static String SUCCESSFUL_REQ = "redirect:./requestCarSharers.do";
+    private static String LIST_CARSHARERS = "listCarSharers";
+    private static int DEFAULT_DISTANCE = 5;
+    private static int MAX_ALLOWED_DISTANCE = 10;
+
+    private int maxDistance;
+    
+    public ListCarSharersController(UserManagementFacade userManagementFacade, String postcodeMapDirName) {
+        this.userManagementFacade = userManagementFacade;
+        this.postcodeMapDirName = postcodeMapDirName;
+    }
+
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+            	    
+        String localUserName = null;
+        User localUser = null;
+        String localPostcode = null;
+        
+        ModelAndView mov = null;
+
+        if (request.getParameter("maxDistance") == null ) {
+            // Request for submission form
+            mov = new ModelAndView(LIST_CARSHARERS);
+            mov.addObject("message", "carsharer.message.submit");
+        } else {
+            // get the full path to the post codes directory
+            postcodesPath = new File(this.getClass().getClassLoader()
+                    .getResource(postcodeMapDirName).toURI()).getAbsolutePath();
+
+            if (request.getUserPrincipal() != null) {
+                localUserName = request.getUserPrincipal().getName();
+                localUser = userManagementFacade.getUser(localUserName);
+
+                localPostcode = localUser.getPostcode();
+
+                if (logger.isDebugEnabled()) {
+                    logger.debug("Checking for carsharers for user: " + localUser.getName() + " with post code: "
+                            + localPostcode);
+                }
+
+                // Check that maxDistance is within allowed range
+                if ( request.getParameter("maxDistance") != null ) {
+                    try {
+                        maxDistance = Integer.parseInt(request.getParameter("maxDistance"));
+                        if (maxDistance < 1) { maxDistance = DEFAULT_DISTANCE; }
+                        if (maxDistance > MAX_ALLOWED_DISTANCE) { maxDistance = MAX_ALLOWED_DISTANCE; }
+                    } catch (NumberFormatException nfe) {
+                        maxDistance = DEFAULT_DISTANCE;
+                    }
+                } else {
+                    maxDistance = DEFAULT_DISTANCE;
+                }
+
+                if (logger.isDebugEnabled()) {
+                    logger.debug("... using maximum distance of: " + maxDistance + "km");
+                }
+
+                if (localPostcode != null && !localPostcode.equals("")) {
+                    // We have a post code for the user
+
+                    // Now lookup all other available post codes for all users (not including this one!)
+
+                    // Get all users
+                    ArrayList<User> allUsers = (ArrayList) userManagementFacade.getUsers();
+                    ArrayList<User> carsharers = new ArrayList<User>();
+
+                    if (logger.isDebugEnabled()) {
+                        logger.debug("Found " + allUsers.size() + " users in database");
+                    }
+
+                    for (User remoteUser: allUsers) {
+                        if (remoteUser.getUsername().equals(localUserName)) {
+                            // Don't compare user with him/herself!
+                            continue;
+                        }
+                        String remotePostcode = remoteUser.getPostcode();
+                        if (logger.isDebugEnabled()) {
+                            logger.debug("User: " + remoteUser.getName() + "; post code: " + remotePostcode);
+                        }
+
+                        if (remotePostcode != null && !remotePostcode.equals("")) {
+                            // Check if post code within specified distance
+                            if (logger.isDebugEnabled()) {
+                                logger.debug("Checking distance for: " + remoteUser.getName());
+                            }
+                            if ( isWithinRange(localPostcode, remotePostcode) ) {
+                                    carsharers.add(remoteUser);
+                            }
+                        }
+                    }
+                    if (carsharers.isEmpty()){
+                        mov = new ModelAndView(LIST_CARSHARERS);
+                        mov.addObject("message", "carsharer.message.nocarsharers");
+                    } else {
+                        mov = new ModelAndView(SUCCESSFUL_REQ);
+                        mov.addObject("carsharers",carsharers);
+                    }
+                } else {
+                    // No postcode for this user so return error message
+                    mov = new ModelAndView(LIST_CARSHARERS);
+                    mov.addObject("message", "carsharer.message.nopostcode");
+                }
+
+
+
+            }
+        }
+
+        return mov;
+    }
+    
+    private boolean isWithinRange ( String postcode1, String postcode2 ) throws URISyntaxException {
+    	
+    	// Tidy up postcodes
+    	postcode1 = postcode1.trim();
+        postcode2 = postcode2.trim();
+        postcode1 = postcode1.toLowerCase();      
+        postcode2 = postcode2.toLowerCase();
+        // May have e.g. 'B1  4HZ'
+        postcode1 = postcode1.replaceAll("\\s+", " ");
+        postcode2 = postcode2.replaceAll("\\s+", " ");
+
+        // Fetch relevant post code file(s) (may have bs12 8pt or b1 8pt - need first one or two letters)
+        String[] matches1 = postcode1.split("\\d", 2);
+        String postcode1FileName = matches1[0] + ".csv";
+        String[] matches2 = postcode1.split("\\d", 2);
+        String postcode2FileName = matches2[0] + ".csv";
+
+        String northing1 = null;
+        String northing2 = null;
+        String easting1 = null;
+        String easting2 = null;
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("Fetching 1st post code file: " + postcodesPath + "/" + postcode1FileName);
+        }
+
+        try {
+            FileReader fr = new FileReader(postcodesPath + "/" + postcode1FileName);
+            BufferedReader br = new BufferedReader(fr);
+            if (logger.isDebugEnabled()) {
+                logger.debug("Looking for post code: " + postcode1);
+            }
+            String line;
+            while ( (line=br.readLine()) != null ) {
+                String[] fields = line.split(",");
+                String postcode = fields[0];
+                postcode = postcode.replaceAll("\\s+", " ");
+            //    if (logger.isDebugEnabled()) {
+            //        logger.debug("Considering post code: " + postcode);
+            //    }
+                if (postcode1.equalsIgnoreCase(postcode)) {
+                    if (logger.isDebugEnabled()) {
+                        logger.debug("Found post code");
+                    }
+                    // Got our 1st post code - get coords
+                    easting1 = fields[1];
+                    northing1 = fields[2];
+                    break;
+                }
+            }
+            br.close();
+            fr.close();
+
+            if (logger.isDebugEnabled()) {
+                logger.debug("easting1: " + easting1 + "; northing1: " + northing1);
+                logger.debug("Fetching 2nd post code file: " + postcodesPath + "/" + postcode2FileName);
+            }
+
+            fr = new FileReader(postcodesPath + "/" + postcode2FileName);
+            br = new BufferedReader(fr);
+
+            if (logger.isDebugEnabled()) {
+                logger.debug("Looking for post code: " + postcode2);
+            }
+            while ( (line=br.readLine()) != null ) {
+                String[] fields = line.split(",");
+                String postcode = fields[0];
+                postcode = postcode.replaceAll("\\s+", " ");
+                if (postcode2.equalsIgnoreCase(postcode)) {
+                    // Got our 2nd post code - get coords
+                    easting2 = fields[1];
+                    northing2 = fields[2];
+                    break;
+                }
+            }
+            br.close();
+            fr.close();
+
+            if (logger.isDebugEnabled()) {
+                logger.debug("easting2: " + easting2 + "; northing2: " + northing2);
+            }
+
+        } catch (FileNotFoundException ex) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("Error fetching file: " + ex.getMessage());
+            }
+            return false;
+        } catch (IOException ex) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("Error fetching file: " + ex.getMessage());
+            }
+            return false;
+        }
+
+        if (northing1 == null || northing2 == null || easting1 == null || easting2 == null ) {
+            return false;
+        }
+
+        // Calculate distances from coords
+        try {
+            int intNorthing1 = Integer.parseInt(northing1);
+            int intNorthing2 = Integer.parseInt(northing2);
+            int intEasting1 = Integer.parseInt(easting1);
+            int intEasting2 = Integer.parseInt(easting2);
+
+            int northingDiff = intNorthing1 - intNorthing2;
+            int eastingDiff = intEasting1 - intEasting2;
+
+            int hypotsquare = (northingDiff * northingDiff) + (eastingDiff * eastingDiff);
+            double distance = Math.sqrt( (double)hypotsquare );
+
+            if (logger.isDebugEnabled()) {
+                logger.debug("Calculated distance: " + distance + " metres");
+            }
+
+            return (distance <= (double)maxDistance*1000);
+
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+    	
+    }
+
+}
---
+++ branches/GreeningEvents/crew/CrewWeb/src/main/webapp/WEB-INF/jsp/listCarSharers.jsp
@@ -0,0 +1,88 @@
+<?xml version="1.0"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title><fmt:message key="carsharer.find.title"/></title>
+    <style type="text/css" media="screen">@import "${pageContext.request.contextPath}/style.css";</style>
+</head>
+
+<body>
+
+<div id="container">
+
+    <%-- banner navigation--%>
+    <%-- <%@ include file="includes/topNavLimited.jsp" %> --%>
+
+    <%-- the logo banner --%>
+    <%--<%@ include file="includes/logo.jsp" %>--%>
+
+    <%-- the logo banner --%>
+    <%--<%@ include file="includes/logo.jsp" %>--%>
+    <div id="headerContainer">
+
+        <div id="headerLogo">
+            <a href="../"><img
+                    src="http://www.jiscdigitalmedia.ac.uk/images/site/logo.gif"
+                    alt="JISC Digital Media Logo"
+                    width="279" height="55"
+                    style="margin-bottom: 2em"/></a>
+        </div>
+
+    </div>
+
+    <%-- The main content --%>
+    <div id="mainBody">
+
+
+        <div class="carsharers-submission-container">
+
+            <form:form action="./listCarSharers.do" method="post">
+
+                <%-- FIND CAR SHARERS --%>
+
+                <fieldset>
+                    <legend><strong>Find delegates to car share with</strong></legend>
+
+                    <p><fmt:message key="carsharers.description"/></p>
+                    
+                    <p>
+                        <select name="maxDistance">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5" selected="selected">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                        </select>
+                    </p>
+
+                    <p>
+                        ${message}
+                    </p>
+
+                    <p>
+                        <input type="submit" name="findCarsharers" value="Search for car sharers"/>
+                        <input type="submit" value="Cancel"/>
+                    </p>
+                </fieldset>
+
+            </form:form>
+
+        </div>
+
+    </div>
+
+    <%-- the logo banner --%>
+<%@ include file="includes/footer.jsp" %>
